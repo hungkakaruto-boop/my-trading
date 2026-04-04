98796cbf99c352b945be2e0e77b46f46cbc31866
hungkakaruto-boop <hungkakaruto@gmail.com>
2026-04-04 22:54:14 +0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -20,7 +20,7 @@ import requests
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
-from vnstock import stock_historical
+from vnstock3 import vnstock
 from datetime import datetime, timedelta
 
 
